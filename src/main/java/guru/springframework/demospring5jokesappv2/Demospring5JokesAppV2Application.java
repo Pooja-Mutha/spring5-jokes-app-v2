cb7dba5a1915ff5ec07616be3fe728fde596a23f
Pooja Mutha <pooja.mutha@netcracker.com>
2021-03-04 16:21:43 +0530

    slide 56

---
+++ src/main/java/guru/springframework/demospring5jokesappv2/Demospring5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.demospring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring5JokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospring5JokesAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/demospring5jokesappv2/Demospring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.demospring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
